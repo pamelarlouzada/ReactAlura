c30179206e1972bb4019c7899d26e17651c3efab
Pamela Rodrigues Louzada <pamela.rlouzada@gmail.com>
2024-01-14 20:40:04 -0300

    Criando Botão

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,3 +1,4 @@
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
@@ -13,7 +14,8 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-                <ListaSuspensa label="Time" itens={times}></ListaSuspensa>
+                <ListaSuspensa label="Time" itens={times} />
+                <Botao>Criar Card</Botao>
             </form>
         </section>
     )
